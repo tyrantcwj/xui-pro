89aa4c3fb6356ab21b59fd56176f1fc13d8e5ed7
iTyc. <38779779+tyrantcwj@users.noreply.github.com>
2026-06-02 12:42:26 +0800

    Wire master to Xray manager

--- cmd/xuid/main.go
+++ cmd/xuid/main.go
@@ -9,6 +9,7 @@ import (
 	"xui-next/internal/master"
 	"xui-next/internal/reality"
 	"xui-next/internal/version"
+	"xui-next/internal/xray"
 )
 
 func main() {
@@ -19,13 +20,15 @@ func main() {
 
 	addr := env("XUI_LISTEN", ":8080")
 	libraryPath := env("XUI_REALITY_LIBRARY", "reality/domains.json")
+	xrayConfig := env("XUI_XRAY_CONFIG", "/usr/local/xui-pro/xray/config.json")
+	xrayService := env("XUI_XRAY_SERVICE", "xray")
 
 	lib, err := reality.Load(libraryPath)
 	if err != nil {
 		log.Fatalf("load reality library: %v", err)
 	}
 
-	srv := master.NewServer(master.NewStore(), lib)
+	srv := master.NewServer(master.NewStore(), lib, xray.NewManager(xrayConfig, xrayService))
 	log.Printf("xuid master listening on %s", addr)
 	if err := http.ListenAndServe(addr, srv.Handler()); err != nil {
 		log.Fatal(err)
